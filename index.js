3d298165a7d83cedd4e825c5ea53fdae1186222d
dlmponda <dlmpondafx@gmail.com>
2020-04-19 23:52:13 +0200

    changed function declearation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 consta add = (x,y) => x+y; 
-consta subtract = (x,y) => x-y; 
+function subtract (x,y){
+ x-y;
+} 
 consta multiply = (x,y) => x*y; 
 consta divide = (x,y) => x/y; 
 consta square = (x,y) => x+y; 
